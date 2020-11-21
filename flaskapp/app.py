97f761863de9e216872cbbf3add321e171edb824
kehindeadigun <36203117+kehindeadigun@users.noreply.github.com>
2020-11-21 22:40:27 +0100

    Changed forms and updated flask app

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,41 +1,47 @@
 from flask import Flask
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, SelectMultipleField
+from wtforms import SubmitField, SelectMultipleField, widgets
 from wtforms.validators import DataRequired
-from mealplan import *
+from mealplan import meal_plan, create_random_meal_plan, get_options
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 users_choices = []
 days = 7
 
-def set_choices(choices):
-    users_choices = choices
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
 
 class ChoicesForm(FlaskForm):
-    choices = SelectMultipleField('Select Proteins', choices=[(protein,protein) for protein in get_options(meal_plan)], validators=[DataRequired()])
+    choices =  MultiCheckboxField('Select Proteins', choices=[(protein,protein) for protein in get_options(meal_plan)], validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET','POST'])
 def home():
     form = ChoicesForm()
-    if form.validate_on_submit():
-        set_choices(form.choices.data)
+    if request.method =="POST" and form.validate_on_submit():
+        users_choices = form.choices.data
         return render_template(url_for(meal_plan))
     return render_template('home.html', form=form)
 
 
 @app.route('/meal-plan')
-def meal_plan():
+def mealplan():
     meal_plan = create_random_meal_plan(meal_plan, users_choices, days)
     return (render_template('mealplan.html', mealplan=meal_plan))
 
 
-@app.error_handler(404)
+@app.errorhandler(404)
 def not_found(e):
     return render_template('404.html')
+
 
 if __name__ == "__main__":
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
+
+
