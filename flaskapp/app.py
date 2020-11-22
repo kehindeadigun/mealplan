d8c1329e3cdc8f44a11b22cfd9141c530ba62dc4
kehindeadigun <36203117+kehindeadigun@users.noreply.github.com>
2020-11-22 09:28:39 +0100

    Updated html and css templates

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -4,9 +4,12 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField, SelectMultipleField, widgets
 from wtforms.validators import DataRequired
 from mealplan import meal_plan, create_random_meal_plan, get_options
+import os
 
+#TEMPLATE_DIR = os.path.abspath('../templates')
+#STATIC_DIR = os.path.abspath('../static')
 
-app = Flask(__name__)
+app = Flask(__name__)#, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 users_choices = []
@@ -17,22 +20,22 @@ class MultiCheckboxField(SelectMultipleField):
     option_widget = widgets.CheckboxInput()
 
 class ChoicesForm(FlaskForm):
-    choices =  MultiCheckboxField('Select Proteins', choices=[(protein,protein) for protein in get_options(meal_plan)], validators=[DataRequired()])
+    choices =  MultiCheckboxField('Select as many Proteins as you like for your meal plan',choices=[(protein,protein) for protein in get_options(meal_plan)])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET','POST'])
 def home():
     form = ChoicesForm()
-    if request.method =="POST" and form.validate_on_submit():
+    if form.validate_on_submit():
         users_choices = form.choices.data
-        return render_template(url_for(meal_plan))
+        return render_template(url_for(mealplan))
     return render_template('home.html', form=form)
 
 
-@app.route('/meal-plan')
+@app.route('/mealplan')
 def mealplan():
-    meal_plan = create_random_meal_plan(meal_plan, users_choices, days)
-    return (render_template('mealplan.html', mealplan=meal_plan))
+    #meal_plan = create_random_meal_plan(meal_plan, users_choices, days)
+    return (render_template('mealplan.html'))#, mealplan=meal_plan))
 
 
 @app.errorhandler(404)
