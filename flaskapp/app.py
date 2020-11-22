ed177339bcfc07fbff040f4ff664233789ce682d
kehindeadigun <36203117+kehindeadigun@users.noreply.github.com>
2020-11-22 18:40:03 +0100

    Fixed the custom meal plan to give poper values

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -4,16 +4,17 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField, SelectMultipleField, widgets
 from wtforms.validators import DataRequired
 from mealplan import meal_plan, create_random_meal_plan, get_options
-import os
+from flask_wtf.csrf import CSRFProtect
+import random
 
-#TEMPLATE_DIR = os.path.abspath('../templates')
-#STATIC_DIR = os.path.abspath('../static')
 
-app = Flask(__name__)#, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 
+csrf = CSRFProtect(app)
+
 users_choices = []
-days = 7
+days = 28
 
 class MultiCheckboxField(SelectMultipleField):
     widget = widgets.ListWidget(prefix_label=False)
@@ -27,14 +28,16 @@ class ChoicesForm(FlaskForm):
 @app.route('/home', methods=['GET','POST'])
 def home():
     form = ChoicesForm()
-    if request.method == 'POST' and form.validate_on_submit():
-        users_choices = form.choices.data
-        print(users_choices)
-        return redirect(url_for('mealplan'))
-    elif request.method == 'POST':
-        flash("Validation Failed")
-        print(form.errors)
-    return render_template('home.html', form=form)
+    optionlength = len(get_options(meal_plan))
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            users_choices = form.choices.data
+            print(users_choices)
+            return redirect(url_for('mealplan'))
+        else:
+            flash("Validation Failed")
+            print(form.errors)
+    return render_template('home.html', form=form, options=optionlength)
 
 
 @app.route('/mealplan')
@@ -44,12 +47,10 @@ def mealplan():
     return (render_template('mealplan.html', mealplan=meal_planned, mealorder=order))
 
 
-#@app.errorhandler(404)
-#def not_found(e):
-# return render_template('404.html')
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html')
 
 if __name__ == "__main__":
     app.debug = True
     app.run(host='0.0.0.0', port=5001)
-
-
--- flaskapp/mealplan.py
+++ flaskapp/mealplan.py
@@ -11,17 +11,18 @@ def import_file(file_name):
 
 def get_options(json_plan):
 
-    '''This function converts the height in cm to feet and inches
+    '''This function converts the height in cm to feet and inches 
     Args: takes meal plan dictionary in format (day-> mealtime-> options->[ list of meals]).
-    result: Returns a Set of all available protein options available in the meal plan.
-    '''
+    result: Returns a list of all available protein options available in the meal plan.'''
+
     choices=[]
     for day, _ in json_plan.items():
-        for meal, data in json_plan[day].items():
+        for _, data in json_plan[day].items():
             if data.get('Options'):
                 for option in data.get('Options'):
                     choices.append(option)
-    return(set(choices))
+    choices.sort()
+    return(sorted(set(choices)))
 
 
 def get_random_day(meal_plan):
@@ -41,9 +42,12 @@ def get_random_meal_from(data,search):
     result: Returns a random meal (string) from the data.
     '''
     meal = data.get(search)
-    #generate a random integer between 0 and X. Will return 0 if X is 0.
+    #generate a random integer between 0 and X. random.randint will return 0 if X is 0.
     meal_type = random.randint(0, len(meal)-1)
+    #return a meal using the meal_type value
     return meal[meal_type]
+
+import random
 
 def create_random_meal_plan(meal_plan, users_choices, days):
     '''This creates a meal plan from the meal_plan  dictionary
@@ -52,31 +56,32 @@ def create_random_meal_plan(meal_plan, users_choices, days):
           3. takes the number of days (integer) to randomize the meal plan for.
     result: Returns a dictionary in format (day-> mealtime-> meal).
     '''
+
     custom_meal_plan = {}
     meal_sets = ['Breakfast', 'Lunch', 'Dinner', 'Snacks']
     
-    for daycount in range(days): #number of days
-        if not custom_meal_plan.get(daycount):
+    for daycount in range(days): #number of days to generate the meal plan for
+        if not custom_meal_plan.get(daycount): #if the day does not exist yet create day
             custom_meal_plan[daycount] = {}    
         
         for meal in meal_sets: #go through the mealtimes (breakfast, lunch, dinner, snacks)
-            rand_day = get_random_day(meal_plan) #get the meal from a random day
-            data = meal_plan[rand_day][meal]
+            rand_day = get_random_day(meal_plan) #get the meal info from a random day from the meal_plan
+            meal_info = meal_plan[rand_day][meal]
 
-            if data.get('Options'):
-                choices = [option for option in data.get('Options') if option in users_choices]
+            if meal_info.get('Options'):
+                choices = [option for option in meal_info.get('Options') if option in users_choices]
                 
                 #check if the options match with the users selected protein choices, pick one at random.
                 if choices:
                     choice = random.choices(choices)[0]
-                    custom_meal_plan[daycount][meal] = get_random_meal_from(data,choice)
+                    custom_meal_plan[daycount][meal] = get_random_meal_from(meal_info,choice)
 
                 else:
                     #if the choices do not match with the users selected protein options, go with the default meal
-                    custom_meal_plan[daycount][meal] = get_random_meal_from(data,'Default')
+                    custom_meal_plan[daycount][meal] = get_random_meal_from(meal_info,'Default')
             else:
                 #if there are no options go with the default meal
-                custom_meal_plan[daycount][meal] = get_random_meal_from(data,'Default')
+                custom_meal_plan[daycount][meal] = get_random_meal_from(meal_info,'Default')
     return custom_meal_plan
 
 meal_plan = import_file('meal_plan.json')
